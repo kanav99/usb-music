f880a2c16510bddd9ad908a6ada11fcc89200e0b
Kanav Gupta <kanav0610@gmail.com>
2026-06-14 21:32:51 -0400

    upodate

--- bt_connect_headset.sh
+++ bt_connect_headset.sh
@@ -9,7 +9,7 @@ for i in $(seq 1 $MAX_RETRIES); do
     echo "Attempt $i: connecting to $HEADSET_MAC..."
     if bluetoothctl connect "$HEADSET_MAC" 2>&1 | grep -q "Connection successful"; then
         echo "Connected to headset. Trying to load audio loopback module..."
-        pactl load-module module-loopback source=alsa_input.platform-3f980000.usb.stereo-fallback sink=bluez_sink.07_01_69_96_E4_3B.a2dp_sink latency_msec=100
+        /usr/sbin/runuser -u kanav pactl load-module module-loopback source=alsa_input.platform-3f980000.usb.stereo-fallback sink=bluez_sink.07_01_69_96_E4_3B.a2dp_sink latency_msec=100
         exit 0
     fi
     sleep $RETRY_INTERVAL
